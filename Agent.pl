d496bb73f1d650585e33ebcf3f7f4da1e02a2d6e
sherelynyap <yap.xuan.ying2001@gmail.com>
2022-04-05 18:13:26 +0800

    change files

--- Agent.pl
+++ Agent.pl
@@ -1,23 +1,36 @@
+
+% set dynamic predicates - Informs the interpreter that the definition of the predicate(s) may change during execution
+:- dynamic([
+  world_size/1,	% Size of the world as [X, Y]
+  position/2,		% position as (A, [X, Y]) implying location of A is [X, Y] 
+  wumpus/1,		% Possible position of Wumpus to be inferred from smell
+  noPit/1,		% noPit([X, Y]) means agent is sure there is no pit on [X, Y] cell, inferred from no breeze on adjacent cell(s)
+  noWumpus/1,		% noWumpus([X, Y]) means agent is sure there is no Wumpus on [X, Y] cell, inferred from no smell on adjacent cell(s)
+  maybeVisitLater/2,	% if no adjacent cell to go to, add the current cell as a probable point to visit later and backtrack
+  goldPath/1		% agent stores each path from [1, 1] cell to the Gold retrieved
+]).
 hasarrow(agent, shotArrow).
 
-%% reborn/0 - implements Agent reset after meeting wumpus
+% reborn/0 - implements Agent reset after meeting wumpus 
 reborn:-
-  % make sure to clear any previous facts stored
-  /*
+  /* make sure to clear any previous facts stored - retractall function in-built Prolog
+  
   retractall(wumpus(_)),
-  retractall(noPit(_)),
-  retractall(noWumpus(_)),
-  retractall(maybeVisitLater(_,_)),
-  retractall(goldPath(_)),
+  retractall(noCoins(_)),
+  retractall(hasArrow(_)),
+  retractall(portals(_,_)),
   */
 
-  % initializations
-  init_board,
+  % initializations 
+  init_relativeMap,
   init_agent,
   init_wumpus.
 
 init_agent :-
-    assert(position(Agent, [0,0])). /* position(Object, [X,Y]) => record object position in database */
+  % position(Object, [X,Y]) => change object position in database 
+  assert(position(Agent, [0,0])). 
 
-init_board :-
+init_relativeMap :-
+  assert(world_size([7, 6]))
+
 
--- WWDriver.py
+++ WWDriver.py
@@ -1,3 +1,64 @@
-from pyswip import Prolog
-prolog = Prolog()
-prolog.consult("Agent.pl")
+#from pyswip import Prolog
+#prolog = Prolog()
+#prolog.consult("C:\Users\Yap Xuan Ying\Documents\Prolog\Agent.pl")
+
+'''
+'#' => INDICATE WALL
+'C' => INDICATE COIN
+'A' => INDICATE AGENT
+'W' => INDICATE WUMPUS
+'P' => INDICATE CONFUNDUS PORTAL
+' ' => INDICATE NOTHING/SAFE CELL
+'''
+
+Map = [ ['#', '#', '#' ,'#', '#', '#'],
+        ['#', 'C', ' ' ,' ', 'P', '#'],
+        ['#', 'P', ' ' ,' ', ' ', '#'],
+        ['#', ' ', '#' ,'A', ' ', '#'],
+        ['#', ' ', ' ' ,'P', ' ', '#'],
+        ['#', 'W', ' ' ,' ', ' ', '#'],
+        ['#', '#', '#' ,'#', '#', '#'] ]
+
+# Agent's Knowledge/Estimate of each cell
+def printAgentKB():
+    Wall = False
+    if Wall is True:
+        print("# # #")
+        print("# # #")
+        print("# # #")
+    else:
+        confoundSignal = True
+        stenchSignal = True
+        tingleSignal = True
+        NPCSignal = True
+        guess = 'Wumpus'
+        possibilities ={'Wumpus' : 'W',
+                        'Portal' : 'O',
+                        'rNorth' : '^',
+                        'rEast' : '>',
+                        'rSouth' : 'v',
+                        'rWest' : '<',
+                        'notVisitedSafe' : 's',
+                        'visitedSafe' : 'S',
+                        'Unknown' : '?'}
+        
+        glitterSignal = True
+        bumpSignal = True
+        ScreamSignal = True
+        
+
+        print("%", end=' ') if confoundSignal is True else print(".", end=' ')
+        print("=", end=' ') if stenchSignal is True else print(".", end=' ')
+        print("T", end=' ') if tingleSignal is True else print(".", end=' ')
+        print()
+
+        print("-", end=' ') if NPCSignal is True else print(" ", end=' ')
+        print(possibilities.get(guess,"error"), end=' ')
+        print("-", end=' ') if NPCSignal is True else print(" ", end=' ')
+        print()
+
+        print("*", end=' ') if glitterSignal is True else print(".", end=' ')
+        print("B", end=' ') if bumpSignal is True else print(".", end=' ')
+        print("@", end=' ') if ScreamSignal is True else print(".", end=' ')
+
+printAgentKB()
